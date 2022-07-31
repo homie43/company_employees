641d52b581cc3dd8dbce2b096e9120e612ebe6ac
Vladimir <homie43@yandex.ru>
2022-07-31 13:03:14 +0300

    test props

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -4,9 +4,9 @@ import "./employees-list.css";
 const EmployeesList = () => {
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            <EmployeesListItem name="Steaphen King" salary={300} /> {/* props устанавливаем тут, выводим на стр в ...list-item.js */}
+            <EmployeesListItem name="Vladimir Kopylov" salary={500} />
+            <EmployeesListItem name="Elton John" salary={1200} />
         </ul>
     )
 }
