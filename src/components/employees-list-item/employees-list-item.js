c879ae1943349a8de446c6c8675b2ac39ab532a5
Vladimir <homie43@yandex.ru>
2022-08-03 17:50:32 +0300

    working with the state of the EmployeesListItem component

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,27 +1,50 @@
+import { toHaveValue } from '@testing-library/jest-dom/dist/matchers';
+import { Component } from 'react';
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name, salary, increase}) => { // получаем данные из EmployeesList, деструктуризируем пропс, помещаем на страницу
+// переделал функциональный компонент в классовый, для работы с состоянием
+class EmployeesListItem extends Component { // получаем данные из EmployeesList, деструктуризируем пропс, помещаем на страницу
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+        }
+    }
 
-    const classIncrease = increase ? 'increase' : "";
+    inCrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
 
-    return (
-        <li className={"list-group-item d-flex justify-content-between " + classIncrease}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button" /* иконка куки */
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+    render() {
 
-                <button type="button" /* иконка корзина */
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+        const {name, salary} = this.props;
+        // теперь increase должен приходить не из props, а из state
+        const {increase} = this.state;
+
+        const classIncrease = increase ? 'increase' : "";
+    
+        return (
+            <li className={"list-group-item d-flex justify-content-between " + classIncrease}>
+                <span className="list-group-item-label">{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button" /* иконка куки */
+                        className="btn-cookie btn-sm "
+                        onClick={this.inCrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button type="button" /* иконка корзина */
+                            className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployeesListItem;
